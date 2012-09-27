4f75c75a68d29b270af6ec90cca271738a93ca63
kbonch@gmail.com <kbonch@gmail.com@66c1f563-44ba-4792-017e-c4d297325f39>
2012-09-27 18:23:43 +0000

    Weather icons and Latitude/Longitude location finder

---
+++ ADG Weather/src/com/adg/location/GPSLocation.java
@@ -0,0 +1,26 @@
+package com.adg.location;
+
+import android.R;
+import android.app.Activity;
+import android.content.Context;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+public class GPSLocation extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		
+		
+		
+		
+		
+	}
+
+	
+
+	
+
+}
