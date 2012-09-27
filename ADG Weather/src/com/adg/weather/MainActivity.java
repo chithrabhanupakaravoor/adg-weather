912d947c01e55db92f0218ad91da5fe32fef13de
tannerwilson523@gmail.com <tannerwilson523@gmail.com@66c1f563-44ba-4792-017e-c4d297325f39>
2012-09-27 17:41:00 +0000

    Added the colors.xml and made the home screen, TW

--- ADG Weather/src/com/adg/weather/MainActivity.java
+++ ADG Weather/src/com/adg/weather/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.home_screen);
     }
 
     @Override
